950d9144af509a3865e457275c7abd9ad8972e81
Bogumił Kamiński <bkamins@sgh.waw.pl>
2021-08-29 11:31:11 +0200

    Apply suggestions from code review

--- test/runtests.jl
+++ test/runtests.jl
@@ -562,6 +562,6 @@ end
 
     for signed in (true, false), compress in (true, false), len in (1, 100, 1000)
         x = PooledArray(fill(1, len), signed=true, compress=true);
-        @inferred PooledVector{Int, Int, Vector{Int}} map(identity, x)
+        VERSION >= v"1.6" && @inferred PooledVector{Int, Int, Vector{Int}} map(identity, x)
     end
 end
